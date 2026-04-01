ae04214f737784aaa3d172f121f3af5f9f5bb096
Olia <oliaopanasiukk@gmail.com>
2026-04-01 09:56:25 +0300

    provide project structure

--- api/PowerStore.Infrastructure/PowerStoreDbContext.cs
+++ api/PowerStore.Infrastructure/PowerStoreDbContext.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PowerStore.Domain
+namespace PowerStore.Infrastructure
 {
-    public class Class1
+    internal class PowerStoreDbContext
     {
     }
 }
